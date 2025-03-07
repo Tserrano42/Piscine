d8b566292083a86c7fbe89b23215f11f3a4d2500
Tserrano <thibaud.serranooo@gmail.com>
2025-03-07 21:12:45 +0800

    /ADD ft_puchar done

--- C00/ex00/ft_putchar.c
+++ C00/ex00/ft_putchar.c
@@ -6,7 +6,13 @@
 /*   By: tserrano <tserrano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 20:44:50 by tserrano          #+#    #+#             */
-/*   Updated: 2025/03/07 20:44:51 by tserrano         ###   ########.fr       */
+/*   Updated: 2025/03/07 21:07:24 by tserrano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_puchar(char c)
+{
+	write(1, &c, 1);
+}
