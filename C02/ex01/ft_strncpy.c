7fb7a92d62da041a35a1801d33a936c36ed167b3
Tserrano <thibaud.serranooo@gmail.com>
2025-04-13 20:49:17 +0800

    ADD/ done + normed

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tserrano <tserrano@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/13 20:04:36 by tserrano          #+#    #+#             */
+/*   Updated: 2025/04/13 20:47:45 by tserrano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	int	index;
+
+	index = 0;
+	while (index <= n && src[index] != '\0')
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	while (index < n)
+	{
+		dest[index] = '\0';
+		index++;
+	}
+	return (dest);
+}
