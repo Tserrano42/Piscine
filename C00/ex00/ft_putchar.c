5c858304a4d95ea84844931f3d2a971cc42c6d6e
Tserrano <thibaud.serranooo@gmail.com>
2025-04-27 18:32:17 +0800

    /UPDATE

--- C00/ex00/ft_putchar.c
+++ C00/ex00/ft_putchar.c
@@ -6,13 +6,13 @@
 /*   By: tserrano <tserrano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 20:44:50 by tserrano          #+#    #+#             */
-/*   Updated: 2025/03/07 21:07:24 by tserrano         ###   ########.fr       */
+/*   Updated: 2025/04/27 18:32:08 by tserrano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_puchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
