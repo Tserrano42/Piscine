c47cf5b47a0ead419a50d7d20b69b92f8e21e5af
Tserrano <thibaud.serranooo@gmail.com>
2025-05-01 19:46:36 +0800

    /ADD done + normined

---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tserrano <tserrano@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 19:39:41 by tserrano          #+#    #+#             */
+/*   Updated: 2025/05/01 19:45:09 by tserrano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		index++;
+	}
+	write(1, str, index);
+}
+
+int	main(int argc, char **argv)
+{
+	int	index;
+
+	index = 1;
+	while (index < argc)
+	{
+		ft_putstr(argv[index]);
+		ft_putstr("\n");
+		index++;
+	}
+}
