93e50c2751b4fcbe5f78b1fed684495766194899
Tserrano <thibaud.serranooo@gmail.com>
2025-05-10 18:58:52 +0800

    /ADD done + nomined

---
+++ C07/ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tserrano <tserrano@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/04 19:45:02 by tserrano          #+#    #+#             */
+/*   Updated: 2025/05/10 18:58:24 by tserrano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+char	*ft_strdup(char *src)
+{
+	int		index;
+	char	*copy;
+
+	index = -1;
+	copy = NULL;
+	while (src[++index])
+		;
+	copy = malloc(sizeof(char) * (index + 1));
+	index = -1;
+	while (src[++index])
+		copy[index] = src[index];
+	copy[index] = '\0';
+	return (copy);
+}
