dfd9e0359d75f004c8f595761525318115e3eb9c
Tserrano <thibaud.serranooo@gmail.com>
2025-05-10 19:32:29 +0800

    /ADD done + normed

---
+++ C07/ex01/ft_range.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tserrano <tserrano@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/10 19:03:13 by tserrano          #+#    #+#             */
+/*   Updated: 2025/05/10 19:31:59 by tserrano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	*ft_range(int min, int max)
+{
+	int	index;
+	int	save_min;
+	int	*my_variable;
+
+	index = -1;
+	save_min = min - 1;
+	my_variable = malloc(sizeof(int) * (max - min));
+	if (min < max)
+	{
+		while (++save_min < max)
+			my_variable[++index] = save_min;
+	}
+	else
+		return (NULL);
+	return (my_variable);
+}
