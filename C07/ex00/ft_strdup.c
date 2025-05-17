51d3ee6f7282e0f8ef6a5de480e7466aec03711f
Tserrano <thibaud.serranooo@gmail.com>
2025-05-17 19:07:07 +0800

    /FIX correction protection malloc

--- C07/ex00/ft_strdup.c
+++ C07/ex00/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: tserrano <tserrano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 19:45:02 by tserrano          #+#    #+#             */
-/*   Updated: 2025/05/10 18:58:24 by tserrano         ###   ########.fr       */
+/*   Updated: 2025/05/17 19:00:45 by tserrano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ char	*ft_strdup(char *src)
 	while (src[++index])
 		;
 	copy = malloc(sizeof(char) * (index + 1));
+	if (!copy)
+		return (NULL);
 	index = -1;
 	while (src[++index])
 		copy[index] = src[index];
